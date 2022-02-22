dadfd869221094d4dabc58049d414f8373eb8714
Xinjinhan <18183124928@163.com>
2022-02-22 17:41:25 +0800

    normalize shell scripts and report file

--- bin/TestSparkWithTpcds.sh
+++ bin/TestSparkWithTpcds.sh
@@ -6,18 +6,16 @@ selectedQueries=q1,q2,q3
 
 
 # submit benchmark program
-if hadoop fs -test -e /BenchmarkData/Tpcds/tpcds_${datascale}/web_site;then
+if hadoop fs -test -e /BenchmarkData/Tpcds/tpcds_${dataScale}/web_site;then
 location=$(cd "$(dirname "$0")";pwd)
 # spark-submit --class <class> <JAR_PATH> <DATA_SCALE> <QUERY_LIST> <REPORT_LOCATION> <HADOOP_HOST>
-spark-submit --class org.shuhai.spark.sql.perf.tpcds.RunTpcds ${location}/../jars/BenchmarkSuites.jar \
+spark-submit --master yarn --class org.shuhai.spark.sql.perf.tpcds.RunTpcds ${location}/../jars/BenchmarkSuites.jar \
 $dataScale \
 $selectedQueries \
-${location}/../Reports \
+${location}/../reports \
 $HOSTNAME
 
 else
    echo "${dataScale}GB Tpcds data does not exist in HDFS or has broken. Please re-generate it before testing."
    exit 1
 fi
-
-
--- bin/TestSparkWithTpch.sh
+++ bin/TestSparkWithTpch.sh
@@ -6,16 +6,16 @@ selectedQueries=q1,q2,q3
 
 
 # submit benchmark program
-if hadoop fs -test -e /BenchmarkData/Tpch/tpch_${datascale}/region;then
+if hadoop fs -test -e /BenchmarkData/Tpch/tpch_${dataScale}/region;then
 location=$(cd "$(dirname "$0")";pwd)
 # spark-submit --class <class> <JAR_PATH> <DATA_SCALE> <QUERY_LIST> <REPORT_LOCATION> <HADOOP_HOST>
-spark-submit --class org.shuhai.spark.sql.perf.tpch.RunTpch ${location}/../jars/BenchmarkSuites.jar \
+spark-submit --master yarn --class org.shuhai.spark.sql.perf.tpch.RunTpch ${location}/../jars/BenchmarkSuites.jar \
 $dataScale \
 $selectedQueries \
-${location}/../Reports \
+${location}/../reports \
 $HOSTNAME
 
-else
+elsetott
    echo "${dataScale}GB Tpch data does not exist in HDFS or has broken. Please re-generate it before testing."
    exit 1
 fi
