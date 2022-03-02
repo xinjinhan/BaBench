a190443b084471155af2fa595cead1394fdeb54e
Xinjinhan <18183124928@163.com>
2022-03-02 15:56:34 +0800

    normalize shell scripts and report file

---
+++ bin/TestHiveWithTpch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# configurations
+dataScale=500
+selectedQueries=q1,q2,q3
+
+
+# submit benchmark program
+if hadoop fs -test -e /BenchmarkData/Tpch/tpch_${dataScale}/region;then
+
+location=$(cd "$(dirname "$0")";pwd)
+querys=(`echo $selectedQueries | tr ',' ' '`)
+durations=()
+durationSum=0
+
+function runHive() {
+  hive -i $location/../querySamples/tpch/init.sql --database tpch_parquet_$1 -e $location/../querySamples/tpch/$2}
+}
+startTime=`date +'%Y-%m-%d_%H:%M:%S'`
+
+for i in "${!querys[@]}";do
+    startTimeQuery=`date +'%Y-%m-%d_%H:%M:%S'`
+    startSecondsQuery=$(date --date="$startTimeQuery" +%s)
+    runHive $dataScale ${querys[$i]}
+    if [ "$?" -ne 0 ];then
+      echo "Hive: ${dataScale}GB Tpch test failed at Query ${querys[$i]}."
+      exit 1
+    fi
+    endTimeQuery=`date +'%Y-%m-%d_%H:%M:%S'`
+    endSecondsQuery=$(date --date="$endTimeQuery" +%s)
+    durations[$i]=$((startSecondsQuery-endSecondsQuery))
+    durationSum=$((durationSum+durations[$i]))
+done
+
+endTime=`date +'%Y-%m-%d_%H:%M:%S'`
+
+else
+   echo "${dataScale}GB Tpch data does not exist in HDFS or has broken. Please re-generate it before testing."
+   exit 1
+fi
+
+echo "Hive  Tpch  ($querys)  ($durations)  $startTime  $endTime  $durationSum  $dataScale  Succeed" >> $location/../reports/bigbench.report
---
+++ core/src/main/scala/org/BernardX/spark/sql/perf/tpch/RunTpch.scala
@@ -0,0 +1,64 @@
+package org.BernardX.spark.sql.perf.tpch
+
+import org.apache.spark.sql.SparkSession
+
+import java.io.{File, FileWriter}
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object RunTpch {
+  def main(args: Array[String]): Unit = {
+    if (args.length != 4) {
+      System.err.println(
+        s"Usage: $RunTpch <DATA_SCALE> <QUERY_LIST> <REPORT_LOCATION> <HADOOP_HOST>"
+      )
+      System.exit(1)
+    }
+      val scaleFactor: String = args(0)
+      val queryListString: String = args(1)
+      val queryNames: Array[String] = queryListString.split(",")
+      val reportLocation: String= args(2)
+      val hadoopHost: String = args(3)
+      val reportDurationFile = new FileWriter(s"${File.separator}$reportLocation${File.separator}bigbench.report",true)
+      val dateFrame: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss")
+
+    try {
+      val sqlContext = SparkSession
+        .builder()
+        .enableHiveSupport()
+        .getOrCreate()
+        .sqlContext
+
+      val tpch = new TPCH(sqlContext)
+      val databaseName = s"tpch_${scaleFactor}_parquet"
+
+      val resultLocation = s"hdfs://$hadoopHost:9000/BenchmarkData/Tpch/Results"
+      val iteration = 1
+      val queryMap = tpch.queriesMap
+      val timeout = 100000
+      val queries = queryNames.map(queryName => queryMap(queryName.replace("q", "Q")))
+
+      val startTime = dateFrame.format(new Date())
+      sqlContext.sql(s"use $databaseName")
+      val experiment = tpch.runExperiment(
+        queries,
+        iterations = iteration,
+        resultLocation = resultLocation)
+      experiment.waitForFinish(timeout)
+      val stopTime = dateFrame.format(new Date())
+
+      val results = experiment.getFinalResults()
+      val times = results.map(res => res.executionTime.get.toInt).toList
+      val duration = times.sum
+      reportDurationFile.write(s"Spark  TPC-H  ($queryListString)  ${times.mkString("(",",",")")}  $startTime" +
+        s"  $stopTime  $duration  ${scaleFactor}GB  Succeed\n")
+    }
+    catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+    finally {
+      reportDurationFile.close()
+    }
+  }
+}
