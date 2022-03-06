bde2619be812df62ae95d0aebedbb1d5691763ef
Xinjinhan <18183124928@163.com>
2022-03-06 22:18:19 +0800

    add spark configurations

--- bin/GenerateTpcdsData.sh
+++ bin/GenerateTpcdsData.sh
@@ -9,7 +9,7 @@ onlyInitializeMetastore=False
 hadoop fs -mkdir -p /BenchmarkData/Tpcds
 location=$(cd "$(dirname "$0")";pwd)
 #spark-submit --class <class> <JAR_PATH> <DATA_SCALE> <ONLY_GENERATE_METASTORE> <DSAGEN_DIR> <HADOOP_HOST>
-spark-submit --master yarn --num-executors 10 --executor.memory 2g --executor.cores 2 \
+spark-submit --master yarn --num-executors 10 --executor-memory 2g --executor-cores 2 \
 --class org.BernardX.spark.sql.perf.tpcds.GenerateTpcdsData ${location}/../jars/bigbench-core-1.0.jar \
 $dataScale \
 $onlyInitializeMetastore \
