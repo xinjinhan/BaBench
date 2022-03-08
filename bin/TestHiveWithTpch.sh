7907aa870c6dc248c5fdfb8eafffef6a9a7256b8
Xinjinhan <18183124928@163.com>
2022-03-08 22:30:03 +0800

    report output of hive test

--- bin/TestHiveWithTpch.sh
+++ bin/TestHiveWithTpch.sh
@@ -13,20 +13,45 @@ querys=(`echo $selectedQueries | tr ',' ' '`)
 durations=()
 durationSum=0
 
+function timediff() {
+    # time format:date +"%s.%N"
+    start_time=$1
+    end_time=$2
+
+    start_s=${start_time%.*}
+    start_nanos=${start_time#*.}
+    end_s=${end_time%.*}
+    end_nanos=${end_time#*.}
+
+    # end_nanos > start_nanos?
+    # Another way, the time part may start with 0, which means
+    # it will be regarded as oct format, use "10#" to ensure
+    # calculateing with decimal
+    if [ "$end_nanos" -lt "$start_nanos" ];then
+        end_s=$(( 10#$end_s - 1 ))
+        end_nanos=$(( 10#$end_nanos + 10**9 ))
+    fi
+
+    # get timediff
+    time=$(( 10#$end_s - 10#$start_s )).`printf "%03d\n" $(( (10#$end_nanos - 10#$start_nanos)/10**6 ))`
+    typeset time=$(echo "scale=3;$time * 1000" | bc)
+    echo $time | awk -F. '{print $1}'
+}
+
 function runHive() {
   hive -i $location/../querySamples/tpch/init.sql --database tpch_$1_parquet -f $location/../querySamples/tpch/$2
 }
 startTime=`date +'%Y-%m-%d_%H:%M:%S'`
 
 for i in "${!querys[@]}";do
-    startSecondsQuery=$(date +%s)
+    startSecondsQuery=$(date +%s.%N)
     runHive $dataScale ${querys[$i]}.sql
     if [ "$?" -ne 0 ];then
       echo "Falied: Hive ${dataScale}GB Tpch test failed at Query ${querys[$i]}."
       exit 1
     fi
-    endSecondsQuery=$(date +%s)
-    durations[$i]=$((startSecondsQuery-endSecondsQuery))
+    endSecondsQuery=$(date +%s.%N)
+    durations[$i]=$(timediff $startSecondsQuery $endSecondsQuery)
     durationSum=$((durationSum+durations[$i]))
 done
 
